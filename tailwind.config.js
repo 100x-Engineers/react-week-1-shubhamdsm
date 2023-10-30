f93745c14e1adfcbeefe0af3ff3783db55fa65e7
shubham yadav <33717931+shubhamdsm@users.noreply.github.com>
2023-10-30 21:43:32 +0530

    added colors from figma

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "twitter-blue": "#1D98F0",
+        "twitter-blue-hover": "#1871CA",
+        "twitter-blue-disabled": "#1E5D87",
+        success: "#00BE74",
+        error: "#8B141A",
+        "card-fill": "#16181C",
+        "search-bar-fill": "#212327",
+        "button-stroke": "#546A7A",
+        "blue-wash": "#75BEEF",
+        "neutral-50": "#F9F9F9",
+        "neutral-100": "#F4F4F4",
+        "neutral-200": "#E4E4E4",
+        "neutral-300": "#D3D3D3",
+        "neutral-400": "#A2A2A2",
+        "neutral-500": "#737373",
+        "neutral-600": "#525252",
+        "neutral-700": "#404040",
+        "neutral-800": "#262626",
+        "neutral-900": "#171717",
+        "neutral-950": "#0A0A0A",
+        "neutral-1000": "#000000",
+      },
+    },
   },
   plugins: [],
-}
-
+};
