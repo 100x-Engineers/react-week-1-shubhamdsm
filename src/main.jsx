c7b24e80e2936daefba2faea8ca44e751adaebdb
shubham yadav <33717931+shubhamdsm@users.noreply.github.com>
2023-11-05 08:44:05 +0530

    added register page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Register from "./pages/register/Register.jsx";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Register />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,8 @@
+const Register = () => {
+  return (
+    <main>
+      <h1>this is register/login page</h1>
+    </main>
+  );
+};
+export default Register;
